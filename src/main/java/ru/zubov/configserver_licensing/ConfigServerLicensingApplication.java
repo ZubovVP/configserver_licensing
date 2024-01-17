bd7856ee5f4e97bbe44641d5972cb92c855e87e0
Vitaly Zubov <Zubov.VP@yandex.ru>
2024-01-17 22:37:12 +0300

    Initial commit

---
+++ src/main/java/ru/zubov/configserver_licensing/ConfigServerLicensingApplication.java
@@ -0,0 +1,16 @@
+package ru.zubov.configserver_licensing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerLicensingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerLicensingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/zubov/configserver_licensing/ConfigserverLicensingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zubov.configserver_licensing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverLicensingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
