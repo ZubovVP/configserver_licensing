e0853c29de91edce9be7a70320cafacf9d632f4d
Vitaly Zubov <Zubov.VP@yandex.ru>
2024-01-27 22:22:40 +0300

    correct

--- src/main/java/ru/zubov/configserver_licensing/ConfigServerLicensingApplication.java
+++ src/main/java/ru/zubov/configserver_licensing/ConfigServerLicensingApplication.java
@@ -1,7 +1,6 @@
 package ru.zubov.configserver_licensing;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
